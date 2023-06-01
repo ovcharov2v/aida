7b6489de838fb1e9b85cab82c20dcfe88ab46892
Viktor Ovcharov <ovcharov.2v@gmail.com>
2023-06-02 03:47:58 +0400

    Fixes

--- src/js/sections/section-image-back.js
+++ src/js/sections/section-image-back.js
@@ -10,16 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	const boxList = gsap.utils.toArray('.section-image-back')
 
 	boxList.forEach((box) => {
-		/*const img = box.querySelector('.section-image-back__bg')
-		gsap.to(img, {
-			scrollTrigger: {
-				trigger: box,
-				scrub: 1.3,
-				invalidateOnRefresh: true
-			},
-			backgroundPosition: "center top -"+(img.offsetHeight)/10+100+'px',
-			ease: "none",
-		});*/
 		const img = box.querySelector('.section-image-back__bg')
 		gsap.fromTo(img, {
 			yPercent: -10,
